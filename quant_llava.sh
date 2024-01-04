7e6fc00eca73d6eede18f2af6d8f996ac8c63868
Chengruidong Zhang <chengzhang@microsoft.com>
2024-01-04 09:02:31 +0000

    Set group size to 128

--- quant_llava.sh
+++ quant_llava.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# 4.31
 
-# python llama.py none textvqa \
-#     --llava /home/chengzhang/models/llava/llava-v1.5-7b \
-#     --wbits 4 --true-sequential --act-order --groupsize -1 --save models/llava-1.5-7b-textv-4bit.pt
+python llama.py none textvqa \
+    --llava /home/chengzhang/models/llava/llava-v1.5-7b \
+    --wbits 4 --true-sequential --act-order --groupsize 128 --save models/llava-1.5-7b-textv-4bit-128.pt
 
 python llama.py /home/chengzhang/models/llava/llava-v1.5-7b c4 \
-    --wbits 4 --true-sequential --act-order --groupsize -1 --save models/llava-1.5-7b-c4-4bit.pt
+    --wbits 4 --true-sequential --act-order --groupsize 128 --save models/llava-1.5-7b-c4-4bit-128.pt
